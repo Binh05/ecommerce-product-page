1a000eb527e7ee29b92c343170bfd14cbf17ed6e
Binh05 <binhdt0452@gmail.com>
2025-07-03 21:49:02 +0700

    update code

--- script.js
+++ script.js
@@ -37,3 +37,64 @@ nextBtn.addEventListener('click', () => {
     currentPosition = Math.max(-step*(totalSldie - 1), currentPosition - step)
     updatePosition()
 })
+
+// cart button 
+
+const cartBtn = document.querySelector('.cart')
+const cartBox = document.querySelector('.cart-container')
+
+cartBtn.addEventListener('click', () => {
+    cartBox.classList.toggle('active')
+})
+
+// quantity product
+
+const numProduct = document.querySelector('.quantity p')
+let currentNum = Number(numProduct.textContent)
+
+const minusBtn = document.querySelector('.minus')
+const plusBtn = document.querySelector('.plus')
+
+const changeNum = (num) => {
+    currentNum = Math.max(currentNum + num, 0)
+    numProduct.textContent = currentNum
+}
+
+minusBtn.addEventListener('click', () => {
+    changeNum(-1)
+})
+
+plusBtn.addEventListener('click', () => {
+    changeNum(1)
+})
+
+// add product to cart
+
+const addBtn = document.querySelector('.add-cart')
+const cartItems = document.querySelector('.cart-items')
+const emptyText = document.querySelector('.empty')
+console.log(emptyText)
+addBtn.addEventListener('click', () => {
+    if (currentNum === 0) return
+    const price = document.querySelector('.price-sale h2').textContent.replace('$', '')
+    emptyText.classList.add('hide')
+    cartItems.innerHTML += `<div class="product-item">
+            <div class="cart-product">
+            <div class="info-product">
+                <img src="./images/image-product-1-thumbnail.jpg" alt="product 1 thumbnail">
+                <div class="name-price">
+                <p>Fall Limited Edition Sneakers</p>
+                <p>$${price} x <span id="num">${currentNum}</span> <span id="total-price">$${(currentNum*Number(price)).toFixed(2)}</span></p>
+                </div>
+            </div>
+            <button type="button" class="delete">
+                <img src="./images/icon-delete.svg" alt="trash to delete product cart" class="trash">
+            </button>
+            </div>
+            <button class="checkout" type="button">Checkout</button>
+        </div>`
+    
+    console.log(emptyText)
+    currentNum = 0;
+    numProduct.textContent = currentNum
+})
